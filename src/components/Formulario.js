f0c1584dab27f215ac20bf559cd3d752402e4975
Alejandro-Ochando <alejandro.ochando@live.com>
2020-09-15 14:45:16 +0200

    Initial commit

---
+++ src/components/Formulario.js
@@ -0,0 +1,58 @@
+import React, {Fragment} from 'react';
+
+const Formulario = () => {
+    return ( 
+        <Fragment>
+            <h2>Crear Cita</h2>
+            <form>
+                <label>Nombre Mascota</label>
+                <input
+                    type="text"
+                    name="mascota"
+                    className="u-full-width"
+                    placeholder="Nombre Mascota"
+                ></input>
+
+                <label>Nombre Dueño</label>
+                <input
+                    type="text"
+                    name="propietario"
+                    className="u-full-width"
+                    placeholder="Dueño de la mascota"
+                ></input>
+
+                <label>Fecha</label>
+                <input
+                    type="date"
+                    name="fecha"
+                    className="u-full-width"
+                    
+                ></input>
+
+                <label>Hora</label>
+                <input
+                    type="time"
+                    name="hora"
+                    className="u-full-wifth"
+                    
+                ></input>
+
+                <label>Sintomas</label>
+                <textarea
+                    className="u-full-width"
+                    name="sintomas"
+
+                ></textarea>
+                
+                <button
+                   type="submit"
+                   className="u-full-width button-primary"
+                >Agregar Cita</button>
+
+            </form>
+        </Fragment>
+
+     );
+}
+ 
+export default Formulario;
